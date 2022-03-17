8e20e1538ffc5e0b6d4f66fed0e0d6a234a8902b
RamanaBotta <ramanab@bosonmotors.com>
2022-03-17 17:05:41 +0530

    removed id from

--- scripts/markers_visualization.py
+++ scripts/markers_visualization.py
@@ -9,7 +9,7 @@ dist_front_rear_wheels = rospy.get_param('/vehicle/dist_front_rear_wheels', 1.5)
 
 
 def vehicle_pose_callback(data):
-    global vehicle_pose_marker, vehicle_pose_id
+    global vehicle_pose_marker
     marker = Marker(
         type=Marker.CUBE,
         id=0,
@@ -18,22 +18,20 @@ def vehicle_pose_callback(data):
         scale=Vector3(wheel_base+0.5, dist_front_rear_wheels, 1),
         header=Header(frame_id='map'),
         color=ColorRGBA(1, 1, 0, 0.5))
-    vehicle_pose_id += target_id
     rospy.logdebug("vehicle_pose_callback")
     vehicle_pose_marker.publish(marker)
 
 
 def target_pose_callback(data):
-    global target_pose_marker, target_id
+    global target_pose_marker
     marker = Marker(
         type=Marker.SPHERE,
-        id=target_id,
+        id=0,
         lifetime=rospy.Duration(1),
         pose=data.pose,
         scale=Vector3(0.3, 0.3, 0.3),
         header=Header(frame_id='map'),
         color=ColorRGBA(255, 255, 255, 1))
-    target_id += target_id
     rospy.logdebug("target_pose_callback")
     target_pose_marker.publish(marker)
 
@@ -45,5 +43,4 @@ if __name__ == "__main__":
     rospy.Subscriber("/target_pose", PoseStamped, target_pose_callback)
     vehicle_pose_marker = rospy.Publisher("/vehicle_pose_marker", Marker, queue_size=2)
     target_pose_marker = rospy.Publisher("/target_pose_marker", Marker, queue_size=2)
-    target_id, vehicle_pose_id = 0, 0
     rospy.spin()
