540093cad9b9689c303879356143b41b76f87f8e
RamanaBotta <ramanab@bosonmotors.com>
2022-06-22 17:15:12 +0530

    docs: added readme file and changed few params file

--- vehicle_common/scripts/mavros_map_to_base.py
+++ vehicle_common/scripts/mavros_map_to_base.py
@@ -27,23 +27,16 @@ front_overhang = rospy.get_param("/vehicle/dimensions/front_overhang", 1.0)
 rear_overhang = rospy.get_param("/vehicle/dimensions/rear_overhang", 0.7)
 rear_axle_center_height_from_ground = rospy.get_param("/vehicle/dimensions/tyre_radius", 0.3)
 
-vehicle_frame = rospy.get_param("vehicle/robot_origin_frame", "base_link")
-odom_topic = rospy.get_param("/tf/odom_topic", '/mavros/local_position/odom')
-send_odom = rospy.get_param("/tf/send_odom", True)  # send odom from base link
-send_foot_print = rospy.get_param("/tf/send_foot_print", True)
-send_odom_topic_name = rospy.get_param("/tf/sent_odom_topic_name", "/vehicle/odom")
+vehicle_frame = rospy.get_param("/base_frame", "base_link")
+odom_in_topic = rospy.get_param("/odometry_in", '/mavros/local_position/odom')
+send_odom = rospy.get_param("/send_odom", True)  # send odom from base link
+send_foot_print = rospy.get_param("/send_footprint", True)
+send_odom_topic_name = rospy.get_param("/odometry_out", "/vehicle/odom")
+send_footprint_topic_name = rospy.get_param("/foorprint_out", "/vehicle/foot_print")
 tf_broad_caster = tf2_ros.TransformBroadcaster()
 tf_msg = TransformStamped()
 fcu_offset_vehicle = (wheel_base + front_axle_to_fcu_position[0])
-# sensor_transform = TransformStamped()
-# sensor_transform.transform.translation = Vector3(-(wheel_base + front_axle_to_fcu_position[0]),
-#                                                  front_axle_to_fcu_position[1],
-#                                                  front_axle_to_fcu_position[2])
-# sensor_transform.transform.rotation = Quaternion(*tf_conversions.transformations.quaternion_from_euler(
-#     front_axle_to_fcu_orientation[0], front_axle_to_fcu_orientation[1], front_axle_to_fcu_orientation[2]))
-# print(sensor_transform.transform)
 
-# footprint specs
 foot_print_specs = [
     [-rear_overhang, -vehicle_width / 2],
     [wheel_base + front_overhang, -vehicle_width / 2],
@@ -54,43 +47,6 @@ foot_print_specs = [
 
 def odom_callback(data):
     """ subscriber for odom, it publishes tf and odom from vehicle base link(rear axil center)"""
-
-    #   tf to from tf_transform
-    # data.pose.pose.position.z = rear_axle_center_height_from_ground
-    # pose_stamp = PoseStamped(pose=data.pose.pose)
-    # # print(pose_stamp)
-    # # print(sensor_transform)
-    # sensor_transform.transform.rotation = data.pose.pose.orientation
-    # transformed_pose_stamp = do_transform_pose(pose_stamp, sensor_transform)
-    # # transformed_pose_stamp =
-    # print("pose_stamp", pose_stamp)
-    # print('transformed_pose_stamp', transformed_pose_stamp)
-    # tf_msg.header.stamp = rospy.Time.now()
-    # tf_msg.header.frame_id = "map"  # data.header.frame_id
-    # tf_msg.child_frame_id = vehicle_frame
-    # tf_msg.transform.translation = Vector3(transformed_pose_stamp.pose.position.x,
-    #                                        transformed_pose_stamp.pose.position.y,
-    #                                        transformed_pose_stamp.pose.position.z)
-    #
-    # tf_msg.transform.rotation = transformed_pose_stamp.pose.orientation
-    # tf_broad_caster.sendTransform(tf_msg)
-    # rospy.logdebug("transform sent")
-
-    # if send_odom:
-    #     odom_msg = Odometry()
-    #     odom_msg = data
-    #     odom_msg.pose.pose.position = transformed_pose_stamp.pose.position
-    #     odom_msg.pose.pose.orientation = transformed_pose_stamp.pose.orientation
-    #     odom_msg.child_frame_id = vehicle_frame
-    #     odom_publisher.publish(odom_msg)
-    #     rospy.logdebug("Odom sent")
-    # #
-    # if send_foot_print:
-    #     polygon_msg = transform_footprint(transformed_pose_stamp)
-    #     foot_prin_pub.publish(polygon_msg)
-    #     rospy.logdebug("footprint sent")
-
-    ### tf to from cos and sign
 
     _, _, yaw = tf_conversions.transformations.euler_from_quaternion(
         [data.pose.pose.orientation.x, data.pose.pose.orientation.y,
@@ -150,5 +106,5 @@ if __name__ == '__main__':
         odom_publisher = rospy.Publisher(send_odom_topic_name, Odometry, queue_size=2)
     if send_foot_print:
         foot_prin_pub = rospy.Publisher("/vehicle/foot_print", PolygonStamped, queue_size=2)
-    rospy.Subscriber(odom_topic, Odometry, odom_callback)
+    rospy.Subscriber(odom_in_topic, Odometry, odom_callback)
     rospy.spin()
