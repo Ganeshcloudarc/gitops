16735b331de037c9c0131dbc2f10191e6310ead8
iam-vishnu <vishnuk@bosonmotors.com>
2022-09-06 12:51:50 +0530

    feat: added stop command w.r.t pilot. - #25

--- vehicle_safety/src/vehicle_safety_analyzer.py
+++ vehicle_safety/src/vehicle_safety_analyzer.py
@@ -7,52 +7,62 @@ import rospy
 from std_msgs.msg import Bool
 from ackermann_msgs.msg import AckermannDrive
 import time
+from pilot.msg import vehicle_stop_command
 OK = DiagnosticStatus.OK
 ERROR = DiagnosticStatus.ERROR
 WARN = DiagnosticStatus.WARN
 
-
 class FailSafeAutoPilot:
     def __init__(self):
         self.fail_status = None
-        self.status = []
+        self.status = {}
         self.whitelist = rospy.get_param('/vehicle_safety/WHITE_LIST')
         self.whitelist = ["vehicle_safety_diagnostics: " + s for s in self.whitelist]
         self.blacklist = rospy.get_param('/vehicle_safety/BLACK_LIST')
         self.blacklist = ["vehicle_safety_diagnostics: " + s for s in self.blacklist]
-        print(self.whitelist)
-        print(self.blacklist)
-        use_vehicle_safety = rospy.get_param("/failsafe_manager/vehicle_safety_diagnostics", True) 
+        use_vehicle_safety = rospy.get_param("/failsafe_manager/vehicle_safety_diagnostics", True)
+        self.stop_cmd_publisher = rospy.Publisher("/vehicle/stop_command", vehicle_stop_command , queue_size=1) 
         if use_vehicle_safety:
             rospy.Subscriber("/vehicle_safety_diagnostics", DiagnosticArray, self.vehicle_safety_diagnose_cb)
 
 
     def vehicle_safety_diagnose_cb(self, data):
+        reason = []
         for field in data.status:
             if field.name in self.whitelist and field.name not in self.blacklist:
                 if field.level == ERROR:
-                    self.status.append(True)
+                    self.status.update({field.name:True})
+                    reason.append(field.name)
                 else:
-                    self.status.append(False)
+                    self.status.update({field.name:False})
             else:
                 # print("Black list",field.name,field.level)
                 pass
-        if any(self.status):
+
+        if any(self.status.values()):
             self.fail_status = True
-            print(True)
         else:
             self.fail_status = False
-            print(False)
-        self.status = []
-    
-    def send_stop_command(self):
-        if self.fail_status:
-            print("Stopping the Vehicle")
-        else:
-            pass
         
+        if self.fail_status:
+            rospy.logerr("Stopping the Vehicle")
+            stop_command_data = vehicle_stop_command()
+            stop_command_data.node = rospy.get_name()
+            stop_command_data.message = "ERROR : {}".format(reason)
+            stop_command_data.status = True
+            rospy.logerr(stop_command_data.message)
+            self.stop_cmd_publisher.publish(stop_command_data)
+        else:
+            stop_command_data = vehicle_stop_command()
+            stop_command_data.node = rospy.get_name()
+            stop_command_data.message = "OK"
+            stop_command_data.status = False
+            # rospy.loginfo("OK from Vehicle Safety")
+            self.stop_cmd_publisher.publish(stop_command_data)
+        self.status = {}
+
 if __name__ == "__main__":
-    rospy.init_node('failsafe_intigator')
+    rospy.init_node('vehicle_safety_analyzer')
     fs = FailSafeAutoPilot()
     rospy.spin()
     
