5b58d84300973decf421982478a88beb90375e52
RamanaBotta <ramanab@bosonmotors.com>
2022-10-11 12:45:52 +0530

    rslidar folder issue

---
+++ sensor_drivers/rslidar_sdk/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/boson/lidar_ws/src/rslidar_sdk
+# Build directory: /home/boson/lidar_ws/src/rslidar_sdk/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("src/rs_driver")
