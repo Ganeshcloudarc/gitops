7250740727177dcdc0bbbba37dc1de3551dbfcfe
Anish Mathew Oommen P <mathewanish2002@gmail.com>
2023-07-07 11:16:55 +0530

    Quick Fixes on Autopilot
    * Fix issue #200: Removed the jsk_plugines from cMakeList
    
    * Updated the hardware in loop unless part
    
    * Fix issue #203: Changed the Robot speed Formula
    
    * changed the diagnostics_period from 0.1 to 0.2 in vehicle_safety_cpp.launch
    
    * Modified the smooth_path param and removed the jvz-plugins
    
    * Fixes #204 -Fixed the numpy issue

--- obstacle_stop_planner/nodes/obstacle_stop_planner.py
+++ obstacle_stop_planner/nodes/obstacle_stop_planner.py
@@ -10,6 +10,7 @@ try:
     import numpy as np
     from sklearn.neighbors import KDTree
     import copy
+    np.float = np.float64
     import ros_numpy
     import rospy
     import tf2_ros
@@ -416,7 +417,7 @@ class ObstacleStopPlanner:
 
     def odom_callback(self, data):
         self.robot_pose = data.pose.pose
-        self.robot_speed = math.hypot(data.twist.twist.linear.x ** 2, data.twist.twist.linear.y ** 2)
+        self.robot_speed = math.sqrt(data.twist.twist.linear.x ** 2, data.twist.twist.linear.y ** 2)
         # print("self.robot_speed", self.robot_speed)
         self.odom_data_in_time = time.time()
         pose_heading = get_yaw(data.pose.pose.orientation)
