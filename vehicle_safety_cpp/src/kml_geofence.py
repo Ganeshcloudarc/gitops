69e515f543097a97f8f1c210c2adaec85bba133a
Jaisreeboson <kjaisree@bosonmotors.com>
2023-07-11 14:57:13 +0530

    fix: add support when geofence.kml doesnt exist (#237)
    * fix: add support when geofence.kml doesnt exist
    
    * feat: using signal_shutdown to exit
    
    * chore : logging
    #232
    
    ---------
    
    Co-authored-by: iam-vishnu <vishnuk@bosonmotors.com>

--- vehicle_safety_cpp/src/kml_geofence.py
+++ vehicle_safety_cpp/src/kml_geofence.py
@@ -44,31 +44,31 @@ class KmlToGeofence:
 
                         else: 
                             rospy.logerr(f"KML file format is not LineString")
-                                                       
+
+                if (self.size_kml != 0):
+                    self.geofence = self.kml_coords
+                    rospy.set_param('/vehicle_safety/geo_fence_coordinates',self.geofence)
+                
+                # when multiple polygon of coordinates are selected
+                else:
+                    for i in range(0,len(self.kml_list)):
+                        poly = Polygon(self.kml_list[i]) 
+                        Area_ = poly.area 
+                        self.Area.append(Area_)
+                        max_area = max(self.Area) 
+                        index = self.Area.index(max_area) 
+                    self.geofence = self.kml_list[index]
+                    rospy.set_param('/vehicle_safety/geo_fence_coordinates', self.geofence)
+                    self.kml_list.pop(index) 
+                    rospy.set_param('/vehicle_safety/no_go_zone_coordinates', self.kml_list)
+                    rospy.loginfo("Geofence params set successful from geofence.kml")
+                self.cb_clt()
+        except FileNotFoundError as err:
+            rospy.logwarn(f"Using Default geofence params. geofence.kml not found in {kml_file_dir}")  
+            rospy.signal_shutdown("File not found"+ str(err))
         except Exception as err:
-            rospy.logerr(f"Error in reading: {self.kml_file_dir}, err : {err}") 
-            rospy.logdebug(f"length of kml way points: {len(self.kml_coords)}")
-        
-        # when only geofence coordinates is selected
-        if (self.size_kml != 0):
-            self.geofence = self.kml_coords
-            rospy.set_param('/vehicle_safety/geo_fence_coordinates',self.geofence)
-        
-        # when multiple polygon of coordinates are selected
-        else:
-            for i in range(0,len(self.kml_list)):
-                poly = Polygon(self.kml_list[i]) 
-                Area_ = poly.area 
-                self.Area.append(Area_)
-                max_area = max(self.Area) 
-                index = self.Area.index(max_area) 
-            self.geofence = self.kml_list[index]
-            rospy.set_param('/vehicle_safety/geo_fence_coordinates', self.geofence)
-            self.kml_list.pop(index) 
-            rospy.set_param('/vehicle_safety/no_go_zone_coordinates', self.kml_list)
-            rospy.loginfo("params are fixed successfully")
-        self.cb_clt()
-    
+            rospy.logwarn(f'Exception in kml_geofence.py: {err}')
+            rospy.signal_shutdown("Exception in kml_geofence.py"+ str(err))
     def cb_clt(self):
             # service call for the updating the visuality in rviz
             
