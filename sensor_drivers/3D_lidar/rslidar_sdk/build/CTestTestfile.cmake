fce5f24b479c9420243edcf68256b72284cca371
Vishnu K <100843600+iam-vishnu@users.noreply.github.com>
2023-08-31 14:51:09 +0530

    151 depricate sensor drivers in autopilot asap 1 (#272)
    * added sensor_drivers as submodule

--- sensor_drivers/3D_lidar/rslidar_sdk/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/boson/lidar_ws/src/rslidar_sdk
-# Build directory: /home/boson/lidar_ws/src/rslidar_sdk/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("src/rs_driver")
--- sensor_drivers/3D_lidar/rslidar_sdk/build/src/rs_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/boson/lidar_ws/src/rslidar_sdk/src/rs_driver
-# Build directory: /home/boson/lidar_ws/src/rslidar_sdk/build/src/rs_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
